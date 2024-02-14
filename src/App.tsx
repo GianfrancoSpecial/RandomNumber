4193fb2f3165acb31a0a27b657a3a88d616f6624
GianfrancoSpecial <152266312+GianfrancoSpecial@users.noreply.github.com>
2024-02-14 18:26:47 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import "./App.css";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
+  const [buttonText, setButtonText] = useState<string>("Start");
 
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  const handleDecrement = () => {
-    setCount(count - 1);
+    setButtonText(randomNumber.toString());
   };
 
   return (
-    <div className="App">
-      <h1 style={{ color: count < 0 ? "red" : "black" }}>{count}</h1>
-      <button onClick={handleIncrement}>Aggiungi</button>
-      <button onClick={handleDecrement}>Sottrai</button>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <button onClick={handleClick} style={{ fontSize: "24px" }}>
+        {buttonText}
+      </button>
     </div>
   );
 };
